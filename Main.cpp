e81730d47c955b240d2f92a71244fce43eae80f6
Wojciech Holisz <rts@holisz.pl>
2017-08-20 17:05:15 +0200

    Add mazes and put code into classes.

--- Main.cpp
+++ Main.cpp
@@ -1,114 +1,240 @@
+#include <algorithm>
+#include <cmath>
 #include <deque>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <list>
+#include <memory>
 #include <vector>
 
-enum Type
+class Node
 {
-    Empty = -1,
-    Wall = -2,
-    Start = -3,
-    End = -4,
-    Way = -5
-};
-
-struct Node
-{
+public:
     int x;
     int y;
 
-    int depth;
+    int distance_from_start;
+    int distance_to_end;
 
-    float distance_from_start;
-    float distance_to_end;
+    int type;
 
-    Node(int x = 0, int y = 0, int depth = 0) : x(x), y(y), depth(depth), distance_from_start(0.0f), distance_to_end(0.0f)
+    Node* parent;
+    std::list<Node*> children;
+
+    Node(int x = 0, int y = 0) :
+        x(x), y(y), distance_from_start(0), distance_to_end(0), parent(nullptr)
     {
     }
 };
 
-std::vector<std::vector<int>> maze;
-
-int sizeX;
-int sizeY;
-
-Node startPos;
-Node endPos;
-
-std::deque<Node> nodes;
-
-bool is_valid(const Node& node)
+class Search
 {
-    if (node.x < 0)
+public:
+    std::deque<Node*> opened;
+    std::deque<Node*> closed;
+    std::deque<Node*> path;
+
+    Search(const Node& start, const Node& end) : start(start), end(end)
     {
-        return false;
     }
 
-    if (node.x > sizeX - 1)
+    Node get_start() const
     {
-        return false;
+        return start;
     }
 
-    if (node.y < 0)
+    Node get_end() const
     {
-        return false;
+        return end;
     }
 
-    if (node.y > sizeY - 1)
+    void set_current_as_visited()
     {
-        return false;
+        closed.emplace_back(opened.front());
+        opened.pop_front();
     }
 
-    return true;
-}
+    bool is_opened(const Node& node) const
+    {
+        auto result = std::find_if(opened.begin(), opened.end(), [node](Node* i) {
+            return node.x == i->x && node.y == i->y;
+        });
 
-void loadMaze(const std::string& fileName)
+        return result != opened.end();
+    }
+
+    bool is_closed(const Node& node) const
+    {
+        auto result = std::find_if(closed.begin(), closed.end(), [node](Node* i) {
+            return node.x == i->x && node.y == i->y;
+        });
+
+        return result != closed.end();
+    }
+
+    bool is_path(const Node& node) const
+    {
+        auto result = std::find_if(path.begin(), path.end(), [node](Node* i) {
+            return node.x == i->x && node.y == i->y;
+        });
+
+        return result != path.end();
+    }
+
+    void sort()
+    {
+        std::sort(opened.begin(), opened.end(), [](Node* a, Node* b) {
+            return a->distance_to_end < b->distance_to_end;
+        });
+    }
+
+    bool is_start(const Node& node) const
+    {
+        return start.x == node.x && start.y == node.y;
+    }
+
+    bool is_end(const Node& node) const
+    {
+        return end.x == node.x && end.y == node.y;
+    }
+
+    void add_path(Node* node)
+    {
+        path.emplace_back(node);
+    }
+private:
+    const Node start;
+    const Node end;
+};
+
+class Maze
 {
-    std::ifstream file(fileName, std::ios::binary | std::ios::ate);
-    auto fileSize = file.tellg();
-    file.seekg(0, file.beg);
-    std::vector<char> data(fileSize);
-    file.read(data.data(), fileSize);
-    file.close();
-
-    sizeX = 0;
-    sizeY = 0;
-
-    for (auto i = 0; i < fileSize; i++)
+public:
+    enum Type
     {
-        if (data[i] == '\n')
+        Empty = -1,
+        Wall = -2,
+        Start = -3,
+        End = -4,
+        Way = -5,
+        Visited = -6
+    };
+
+    Maze(const std::string file)
+    {
+        load(file);
+    }
+
+    int get(int x, int y) const
+    {
+        return maze[x][y];
+    }
+
+    Node get_start() const
+    {
+        return start;
+    }
+
+    Node get_end() const
+    {
+        return end;
+    }
+
+    bool is_accessible(int x, int y) const
+    {
+        return get(x, y) == Type::Empty;
+    }
+
+    bool is_accessible(int x_1, int y_1, int x_2, int y_2) const
+    {
+        return false;
+    }
+
+    bool is_valid(int x, int y)
+    {
+        if (x < 0)
         {
-            if (sizeY == 0)
+            return false;
+        }
+
+        if (x > size_x - 1)
+        {
+            return false;
+        }
+
+        if (y < 0)
+        {
+            return false;
+        }
+
+        if (y > size_y - 1)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    void clear_way()
+    {
+        for (auto y = 0; y < size_y; y++)
+        {
+            for (auto x = 0; x < size_x; x++)
             {
-                sizeX = i;
+                if (maze[x][y] == Type::Way)
+                {
+                    maze[x][y] = Type::Visited;
+                }
+            }
+        }
+    }
+
+    void load(const std::string& fileName)
+    {
+        std::ifstream file(fileName, std::ios::binary | std::ios::ate);
+        auto fileSize = file.tellg();
+        file.seekg(0, file.beg);
+        std::vector<char> data(fileSize);
+        file.read(data.data(), fileSize);
+        file.close();
+
+        size_x = 0;
+        size_y = 0;
+
+        for (auto i = 0; i < fileSize; i++)
+        {
+            if (data[i] == '\n')
+            {
+                if (size_y == 0)
+                {
+                    size_x = i;
+                }
+
+                size_y += 1;
+            }
+        }
+
+        maze.resize(size_x, std::vector<int>(size_y));
+
+        auto index = 0;
+
+        for (auto y = 0; y < size_y; y++)
+        {
+            for (auto x = 0; x < size_x; x++)
+            {
+                maze[x][y] = data[index++];
             }
 
-            sizeY += 1;
-        }
-    }
-
-    maze.resize(sizeX, std::vector<int>(sizeY));
-
-    auto index = 0;
-
-    for (auto y = 0; y < sizeY; y++)
-    {
-        for (auto x = 0; x < sizeX; x++)
-        {
-            maze[x][y] = data[index++];
+            index += 1;
         }
 
-        index += 1;
-    }
-
-    for (auto y = 0; y < sizeY; y++)
-    {
-        for (auto x = 0; x < sizeX; x++)
+        for (auto y = 0; y < size_y; y++)
         {
-            switch (maze[x][y])
+            for (auto x = 0; x < size_x; x++)
             {
+                switch (maze[x][y])
+                {
                 case ' ':
                     maze[x][y] = Type::Empty;
                     break;
@@ -118,171 +244,182 @@ void loadMaze(const std::string& fileName)
                     break;
 
                 case 'S':
-                    maze[x][y] = Type::Start;
-                    startPos = Node(x, y);
+                    maze[x][y] = Type::Empty;
+                    start.x = x;
+                    start.y = y;
                     break;
 
                 case 'E':
-                    maze[x][y] = Type::End;
-                    endPos = Node(x, y);
+                    maze[x][y] = Type::Empty;
+                    end.x = x;
+                    end.y = y;
                     break;
-            }
-        }
-    }
-}
-
-void showMaze()
-{
-    system("cls");
-    
-    for (auto y = 0; y < sizeY; y++)
-    {
-        for (auto x = 0; x < sizeX; x++)
-        {
-            switch (maze[x][y])
-            {
-                case Type::Empty:
-                    std::cout << ' ';
-                    break;
-
-                case Type::Wall:
-                    std::cout << static_cast<unsigned char>(219);
-                    break;
-
-                case Type::Start:
-                    std::cout << 'S';
-                    break;
-
-                case Type::End:
-                    std::cout << 'E';
-                    break;
-
-                case Type::Way:
-                    std::cout << static_cast<unsigned char>(178);
-                    break;
-
-                default:
-                    std::cout << static_cast<unsigned char>(177);
-            }
-        }
-
-        std::cout << std::endl;
-    }
-}
-
-bool a_star()
-{
-    maze[startPos.x][startPos.y] = 0;
-    nodes.push_back(startPos);
-
-    while (nodes.size() > 0)
-    {
-        auto node = nodes.front();
-
-        Node indices[4] =
-        {
-            Node(-1,  0),
-            Node( 1,  0),
-            Node( 0, -1),
-            Node( 0,  1)
-        };
-
-        for (auto i = 0; i < 4; ++i)
-        {
-            auto next_node = Node(node.x + indices[i].x, node.y + indices[i].y, node.depth + 1);
-
-            if (maze[next_node.x][next_node.y] == Type::End)
-            {
-                maze[next_node.x][next_node.y] = next_node.depth;
-
-                return true;
-            }
-
-            if (is_valid(next_node))
-            {
-                if (maze[next_node.x][next_node.y] == Type::Empty)
-                {
-                    if (maze[next_node.x][next_node.y] < 0)
-                    {
-                        maze[next_node.x][next_node.y] = next_node.depth;
-
-                        nodes.push_back(next_node);
-                    }
                 }
             }
         }
-
-        nodes.pop_front();
     }
 
-    return false;
-}
-
-bool a_star_path()
-{
-    auto node = Node(endPos.x, endPos.y);
-    auto depth = maze[node.x][node.y];
-
-    maze[node.x][node.y] = Type::Way;
-
-    while (node.x != startPos.x || node.y != startPos.y)
+    void print(const Search& search)
     {
-        Node indices[4] =
+        system("cls");
+
+        for (auto y = 0; y < size_y; y++)
         {
-            Node(-1,  0),
-            Node( 1,  0),
-            Node( 0, -1),
-            Node( 0,  1)
-        };
-
-        auto found = false;
-
-        for (auto i = 0; i < 4; ++i)
-        {
-            auto next_node = Node(node.x + indices[i].x, node.y + indices[i].y);
-            auto next_depth = maze[next_node.x][next_node.y];
-
-            if (is_valid(next_node))
+            for (auto x = 0; x < size_x; x++)
             {
-                if (maze[next_node.x][next_node.y] >= 0)
+                if (search.is_start(Node(x, y)))
                 {
-                    if (next_depth < depth)
+                    std::cout << 'S';
+                }
+                else if (search.is_end(Node(x, y)))
+                {
+                    std::cout << 'E';
+                }
+                else if (search.is_path(Node(x, y)))
+                {
+                    std::cout << static_cast<unsigned char>(178);
+                }
+                else if (search.is_opened(Node(x, y)))
+                {
+                    std::cout << static_cast<unsigned char>(177);
+
+                }
+                else if (search.is_closed(Node(x, y)))
+                {
+                    std::cout << static_cast<unsigned char>(177);
+                }
+                else
+                {
+                    switch (maze[x][y])
                     {
-                        maze[next_node.x][next_node.y] = Type::Way;
+                    case Type::Empty:
+                        std::cout << ' ';
+                        break;
 
-                        node = next_node;
-                        depth = next_depth;
-
-                        found = true;
-
+                    case Type::Wall:
+                        std::cout << static_cast<unsigned char>(219);
                         break;
                     }
                 }
             }
-        }
 
-        if (found == false)
-        {
-            return false;
+            std::cout << std::endl;
         }
+    }
+private:
+    int size_x;
+    int size_y;
+
+    std::vector<std::vector<int>> maze;
+
+    Node start;
+    Node end;
+};
+
+int get_distance(const Node& a, const Node& b)
+{
+    return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+}
+
+std::vector<Node> get_indices()
+{
+    static const std::vector<Node> indices =
+    {
+        Node(-1, -1),
+        Node(-1,  0),
+        Node(-1,  1),
+        Node( 0, -1),
+        Node( 0,  1),
+        Node( 1, -1),
+        Node( 1,  0),
+        Node( 1,  1)
+    };
+
+    return indices;
+}
+
+bool a_star_path(Maze& maze, Search& search)
+{
+    auto node = search.opened.front();
+
+    while (search.is_start(*node) == false)
+    {
+        search.add_path(node);
+
+        maze.print(search);
+
+        node = node->parent;
     }
 
     return true;
 }
 
-int main()
+bool a_star(Maze& maze, Search& search)
 {
-    loadMaze("Maze.txt");
+    search.opened.push_back(new Node(search.get_start()));
 
-    if (a_star())
+    while (search.opened.size())
     {
-        a_star_path();
+        search.sort();
+
+        if (search.is_end(*search.opened.front()))
+        {
+            return true;
+        }
+
+        auto new_node = search.opened.front();
+        auto& node = *new_node;
+
+        for (const auto& i : get_indices())
+        {
+            auto new_next_node = new Node(node.x + i.x, node.y + i.y);
+            auto& next_node = *new_next_node;
+
+            if (maze.is_valid(next_node.x, next_node.y))
+            {
+                if (search.is_closed(next_node) == false)
+                {
+                    if (search.is_opened(next_node) == false)
+                    {
+                        if (maze.is_accessible(next_node.x, next_node.y))
+                        {
+                            next_node.distance_from_start = get_distance(search.get_start(), next_node);
+                            next_node.distance_to_end = get_distance(search.get_end(), next_node);
+
+                            next_node.parent = new_node;
+                            search.opened.emplace_back(new_next_node);
+                        }
+                    }
+                }
+            }
+        }
+
+        search.set_current_as_visited();
+
+        maze.print(search);
     }
 
-    maze[startPos.x][startPos.y] = Type::Start;
-    maze[endPos.x][endPos.y] = Type::End;
+    return false;
+}
 
-    showMaze();
+int main()
+{
+    std::string files[] =
+    {
+        "Maze.txt",
+        "Maze_Closed.txt",
+        "Maze_Empty.txt",
+        "Maze_Room.txt",
+        "Maze_Wall.txt"
+    };
+
+    Maze maze(files[4]);
+    Search search(maze.get_start(), maze.get_end());
+
+    if (a_star(maze, search))
+    {
+        a_star_path(maze, search);
+    }
 
     std::cin.ignore();
 
